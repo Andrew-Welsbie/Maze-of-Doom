691f7f40e2ec3b12ce10457308af46ff1953b79b
Andrew-Welsbie <84402146+Andrew-Welsbie@users.noreply.github.com>
2022-01-09 16:46:34 -0800

    player and terrain

---
+++ Maze Horror Game/Assets/PlayerMovement.cs
@@ -0,0 +1,78 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+
+{
+
+
+
+    public CharacterController controller;
+
+    public float speed = 6f;
+    public float gravity = -9.8f;
+    public float jumpHeight = 3f;
+
+    public Transform groundCheck;
+    public float groundDistance = 0.4f;
+    public LayerMask groundMask;
+
+    Vector3 velocity;
+    bool isGrounded;
+    bool isSprinting = false;
+    bool canChange = true;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey("left shift") && speed < 27 && canChange)
+        {
+            Debug.Log("shift down");
+            if (!isSprinting)
+            {
+                speed += 20f;
+            }
+            else if (isSprinting)
+            {
+                speed -= 20;
+            }
+            canChange = false;
+            isSprinting = !isSprinting;
+
+        }
+        if (Input.GetKeyUp("left shift"))
+        {
+            canChange = true;
+        }
+
+        isGrounded = Physics.CheckSphere(groundCheck.position, groundDistance, groundMask);
+
+        if (isGrounded && velocity.y < 0)
+        {
+            velocity.y = -2f;
+        }
+        float x = Input.GetAxis("Horizontal");
+        float z = Input.GetAxis("Vertical");
+
+        Vector3 move = transform.right * x + transform.forward * z;
+        controller.Move(move * speed * Time.deltaTime);
+
+
+
+        if (Input.GetButtonDown("Jump") && isGrounded)
+        {
+            Debug.Log("sjfhjsd");
+            velocity.y = Mathf.Sqrt(jumpHeight * -2f * gravity);
+        }
+
+
+        velocity.y += gravity * Time.deltaTime;
+
+        controller.Move(velocity * Time.deltaTime);
+
+
+
+    }
+}
+
